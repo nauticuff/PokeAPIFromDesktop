c0aad84142fe7c6856548687bf07f79a289610cf
nauticuff <felipecarlos200x@gmail.com>
2023-02-03 19:04:03 -0800

    stuff done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,105 +1,130 @@
 
 let searchValue;
 let searchBoxValue;
+let trueLocation;
 let pokeData;
 let pokeData1;
 let pokeData2;
-let location2;
-let location3;
+let pokeData3;
+let pokeData4;
 
 let searchBtn = document.querySelector('[data-search-btn]')
 let randomBtn = document.getElementById('rndBtn')
 let searchBox = document.getElementById('searchBox')
 
+let pokemonName = document.querySelector('[data-pokemon-name]');
+let defaultImg = document.querySelector('[data-default-img]')
+let shinyImg = document.querySelector('[data-shiny-img]');
+let abilities = document.querySelector('[data-abilities]');
+let type = document.querySelector('[data-type]');
+let pokeLocation = document.querySelector('[data-poke-location]');
+
 randomBtn.addEventListener('click', () => {
     let rndNum = Math.floor(Math.random() * 649) + 1
     FetchPokemon(rndNum);
-}) 
+})
 
-searchBox.addEventListener("keypress", function(event){
-    if(event.key == "Enter") {
+searchBox.addEventListener("keypress", function (event) {
+    if ((searchBoxValue = document.getElementById('searchBox').value) == "") {
+        return
+    }
+    if (event.key == "Enter") {
         searchBoxValue = document.getElementById('searchBox').value
         FetchPokemon(searchBoxValue);
     }
-  });
 
+});
 
+function RenderData() {
+    pokeData1 = parsePokeData(pokeData);
+    pokeData2 = parseAbilities(pokeData);
+    pokeData3 = parseTypes(pokeData);
+    
+    pokemonName.textContent = pokeData1.name;
+    defaultImg.src = pokeData1.defaultURL;
+    shinyImg.src = pokeData1.shinyURL;
 
-//OLD WAY
-// function FetchPokemon(searchValue){
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}`)
-//     .then(response => response.json())
-//     .then(data => {
-//         pokeData = parsePokeData(data)
-//     }).then( () => {
-//         console.log(pokeData)
-//     })
-// }
+    abilities.innerHTML = "";
+    pokeData2.forEach(ability => {
+        let tag = document.createElement('p')
+        tag.textContent = ability.abilityName
+        tag.className = "text-black text-center text-xl first-letter:capitalize"
+        abilities.append(tag)
+    })
+    type.innerHTML = "",
+        pokeData3.forEach(types => {
+            let tag = document.createElement('p')
+            tag.textContent = types.type
+            tag.className = "text-black text-center text-xl first-letter:capitalize"
+            type.append(tag)
+        })
+    
+    pokeLocation.textContent = trueLocation;
+    
+
+}  
 
 async function FetchPokemon(searchValue) {
     //Call API and save it in json format
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}`)
 
     //save the formatted data in a global variable, should be local though
-    pokeData = await res.json();
-    console.log("ALL DATA BELOW")
-    console.log(pokeData)
+    const data = await res.json();
+    pokeData = data
+    FetchLocation(pokeData);
+    RenderData()
+    
 
-    //parse the data for what is needed
-    console.log("PARSED DATA BELOW")
-    pokeData1 = parsePokeData(pokeData)
-    pokeData2 = parseAbilities(pokeData)
-    //parseLocation()
-    
-    //log the parsed data
-    console.log(pokeData1)
-    console.log(pokeData2)
-    // setTimeout(() => {
-    //     console.log(location2)
-    // }, 100);
-    
-  }
+}
 
 //function to parse data
-function parsePokeData({moves, name, id}){
+function parsePokeData({ moves, name, id, sprites, location_area_encounters}) {
 
     const {
         name: moveName,
     } = moves[0].move
 
+    const {
+        front_default: defaultURL,
+        front_shiny: shinyURL,
+    } = sprites
+
     return {
         name,
         id,
         moveName,
+        defaultURL,
+        shinyURL,
+        location_area_encounters
     }
 }
 
-function parseAbilities({abilities}){
+function parseAbilities({ abilities }) {
     return abilities.map((index) => {
         return {
             abilityName: index.ability.name,
         }
-    }) 
+    })
 }
 
-// async function parseLocation(){
-//     // const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}`)
+function parseTypes({ types }) {
+    return types.map((index) => {
+        return {
+            type: index.type.name,
+        }
+    })
+}
 
-//     // //save the formatted data in a global variable, should be local though
-//     // pokeData = await res.json();
 
-//     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchValue}/encounters`)
-//     location2 = await res.json();
-//     console.log(location2)
-//     location2 = location2[0].location_area.name
 
-//     // .then(response => response.json())
-//     // .then(data => {
-//     //     location2 = data[0].location_area.name;
-//     // })
-//     //location2 = location2.
-//     //console.log(location2)
-    
-// }
+function FetchLocation(pokeData){
+    fetch(pokeData.location_area_encounters)
+        .then(response => response.json())
+        .then(data => {
+            trueLocation = data[0].location_area.name.replace(/-/g, " ")
+        })
+    }
+
+
 
 
